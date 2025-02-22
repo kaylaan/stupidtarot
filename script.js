5facea90ea44a27170cea1d18675604681e1293c
kaylaan <119367482+kaylaan@users.noreply.github.com>
2025-02-22 15:20:23 -0500

    images and categories

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const typewriterElement = document.getElementById('typewriter');
-    const enterButton = document.getElementById('enterButton');
-    const mainContent = document.getElementById('mainContent');
-    const intro = document.getElementById('intro');
-    const text = '#STUPiDTAROT';
+    let typewriterElement = document.getElementById('typewriter');
+    let enterButton = document.getElementById('enterButton');
+    let nameQ = document.getElementById('nameQuestion');
+    let intro = document.getElementById('intro');
+    let category = document.querySelector('#category');
+    let title = '#STUPiDTAROT';
+    let nextButton = document.getElementById('nextButton');
+    let name = document.querySelector('#name')
+    let user = ''
+    let choice;
     let index = 0;
+    
 
     // Function to create typewriter effect
     function typeWriter() {
-        if (index < text.length) {
-            typewriterElement.innerHTML += text.charAt(index);
+        if (index < title.length) {
+            typewriterElement.innerHTML += title.charAt(index);
             index++;
-            setTimeout(typeWriter, 80); // Adjust the speed by changing the timeout value
+            setTimeout(typeWriter, 80); 
         }
     }
 
@@ -22,14 +28,15 @@ document.addEventListener('DOMContentLoaded', function() {
     enterButton.addEventListener('click', function() {
         // Hide the intro page and show the main content
         intro.style.display = 'none';
-        mainContent.style.display = 'block';
+        nameQ.style.display = 'block';
     });
 
     // Add a click event listener to the main button
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', function() {
-        // Code to execute when the button is clicked
-        button.innerText = 'Hello';
-        button.style.backgroundColor = 'blue';
+    
+    nextButton.addEventListener('click', function() {
+        console.log(name.value);
+        user = name.value;
     });
+
+
 });
